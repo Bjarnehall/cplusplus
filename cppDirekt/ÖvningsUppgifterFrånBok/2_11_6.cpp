0cc7d866d07a93e04c336c5f933a1bf46bc4bc85
Jonas Bjarnehall <ssepheron@MSI>
2024-06-23 10:04:18 +0200

    finished game

--- cppDirekt/ÖvningsUppgifterFrånBok/2_11_6.cpp
+++ cppDirekt/ÖvningsUppgifterFrånBok/2_11_6.cpp
@@ -1,7 +1,87 @@
 /*
 Skriv ett program som beräknar hur mycket växel som ska ges tillbaka
-när man hanlat samt i vilka sedlar och mynt växeln ska utbetalas.
+när man handlat samt i vilka sedlar och mynt växeln ska utbetalas.
 
 Indata till programmet skall vara det pris man skall betala samt
-i vilket belopp som ges i betalning.
+ vilket belopp som ges i betalning.
  */
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+
+int main()
+{
+    int totalt_pris, totalt_belopp, summa_vaxel, extra_sedlar;
+    extra_sedlar = 0;
+
+    cout << "" << endl
+         << "Välkomme till Bosses kiosk!" << endl
+         << "==============================================================" << endl
+         << "Vi använder oss här av självutcheckning, ange totala priset " << endl
+         << "på varorna. Mata sedan in sedlar och ange totala beloppet du " << endl
+         << "betalt så får du din växel tillbaka i korekkt valörer " << endl
+         << "--------------------------------------------------------------" << endl
+         << "Ange totalt pris för varor:                           "; cin >> totalt_pris;
+
+    //cin  >> totalt_pris;
+    cout << "--------------------------------------------------------------" << endl
+         << "Det totala priset är      : " << setw(29) << totalt_pris << " kr " << endl
+         << "--------------------------------------------------------------" << endl
+         << "Ange belopp att betala    :                           "; cin >> totalt_belopp;
+
+    //cin  >> totalt_belopp;
+
+    if (totalt_pris > totalt_belopp) {
+        cout << "Du har inte betalt tillräckligt för varorna! " << endl
+             << "===========================================================" << endl
+             << "Priset för varorna är " << setw(35) << totalt_pris << " kr " << endl
+             << "Du har betalt " << totalt_belopp << " kr " << endl
+             << "-----------------------------------------------------------" << endl
+             << "Vänligen mata in extra sedlar " << endl;
+        cin  >> extra_sedlar;
+    }
+    // Beräkning
+    totalt_belopp = totalt_belopp + extra_sedlar;
+    summa_vaxel = totalt_belopp - totalt_pris;
+
+    cout << "---------------------------------------------------------------" << endl
+         << "Du får tillbaka " << summa_vaxel << " kr " << endl;
+
+    /*
+    Dela upp växeln i olika valörer
+    aktuella valörer i kassa systemet
+    100 kr sedel
+    50 kr sedel
+    20 kr sedel
+    10 kr mynt
+    5 kr mynt
+    1 kr mynt
+    */
+    // dela upp summa_vaxel i de olika valörerna där högsta möjliga
+    // valör betlas ut i första hand. Till exempel om:
+    // summa_vaxel = 387 så ges vaxel tillbaka i, 3st 100kr sedlar, 1 st 50 kr sedel, 1 st 20 kr sedel, 1 t 10 kr mynt, 1 st 5kr mynt, 2 st en kr mynt.
+    int valorer[] = {100, 50, 20, 10, 5, 1};
+    int antal_valorer[] = {0, 0, 0, 0, 0, 0,};
+    int antal_valorer_storlek = sizeof(valorer) / sizeof(valorer[0]);
+
+    for (int i = 0; i < antal_valorer_storlek; ++i) {
+        antal_valorer[i] = summa_vaxel / valorer[i];
+        summa_vaxel %= valorer[i];
+    }
+
+    cout << "växeln ges ut i följande valörer:" << endl;
+    for (int i = 0; i < antal_valorer_storlek; ++i) {
+        if (antal_valorer[i] > 0) {
+            cout << antal_valorer[i] << " st " << valorer[i] << " kr" << endl;
+        }
+    }
+
+    return 0;
+}
+
+
+
+
--- roulette/main.cpp
+++ roulette/main.cpp
@@ -1,8 +1,14 @@
-
+/*
+ This is a c++ program for a console based roulette game
+ The program is made as an assignment for a introductory
+ course at His university
+ @author Jonas Bjarnehall
+*/
 
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <ctime>
 
 using namespace std;
 
@@ -16,89 +22,192 @@ int main()
     string playerCol;
     string answerPlay;
     string playType;
+    string spin;
+    string playAgain;
 
-    // Promt to let player know what program is about and
-    // if player wants to proceed.
-    cout << "Welcome to His Casino" << endl
-         << "Here you can play Roulette!" << endl
-         << "" << endl
-         << "Diffrent ways to play:" << endl
-         << "Bet on a number between 1 and 36 " << endl
-         << "Bet on a color Black or Red " << endl
-         << "" << endl
-         << "Do you wanna play?  Answer yes/no" << endl;
-    
-    cin  >> answerPlay;
+    // Let game loop
+    while (true)
+    { 
+         
+         // Promt to let user know what the program is about
+          cout << "" << endl
+               << "       Welcome to His Casino" << endl
+               << "====================================" << endl
+               << "     Here you can play Roulette" << endl
+               << "" << endl
+               << "Diffrent ways to play:" << endl
+               << "Bet on a number:    between 1 and 36" << endl
+               << "Bet on a color:         Black or Red" << endl
+               << "" << endl
+               << "Do you wanna play?     Answer yes/no" << endl;
+          
+          // Let user tell program to proceed or not 
+          cin >> answerPlay;
+               if (cin.eof()) 
+                    return 0;// Let user shut down program
 
-    // Checks if player wants to proceed and if so lets the
-    // player decide which type of game to play.
-    if (answerPlay == "yes") {
-        cout << "" << endl
-             << "You have: " << wallet << " kr in your wallet "  << endl
-             << "Decide if you want to play on color or number " << endl
-             << "The payback is : " << endl
-             << "   x2 on color         To play this type: color" << endl
-             << "   x10 on number       To play this type: number" << endl;
-    } else {
-        cout << "Thank you for the visit bye!" << endl;
-        return 0;
-    }
+          // Checks if user wants to proceed and if so let the user decide path to proceed
+          if (answerPlay == "yes")
+                    {
+                         cout << "" << endl
+                              << "You have: " << wallet << " kr     in your wallet " << endl
+                              << "" << endl
+                              << "Decide if you want to play on color" << endl
+                              << "or a chosen number the payback is :" << endl
+                              << "" << endl
+                              << "   x2 on color         Type:  color" << endl
+                              << "   x10 on number       Type: number" << endl;
+                    }
+                    else
+                    {
+                         cout << "Thank you for the visit bye!" << endl;
+                         return 0; // shut program down if user does not answe yes
+                    }
 
-    // Store variable for type of game to play.
-    cin  >> playType;
-    cout << "" << endl
-         << "You hace chosen to play on: " << playType << endl;
+          // Let user input type of play and store that value
+          cin >> playType;
+                    if (cin.eof()) 
+                         return 0;// Let user shut down program
 
-    // Handle diffrent types of play depending on user actions.
-    if (playType == "color") {
-        cout << "Select a color to bet on " << endl
-             << "   To bet on Black type: black" << endl
-             << "   To bet on Red   type: red" << endl;
-        cin  >> playerCol;
-        cout << "" << endl
-             << "You have chosen the color :" << playerCol << endl
-             << "" << endl;
+               //Let user know wich type of play that is chosen
+          cout << "" << endl
+               << "You hace chosen to play on a " << playType << endl;
 
-    } else if (playType == "number") {
-        cout << "Select a number to bet on " << endl
-             << "Type a number between 1 and 36: " << endl;
-        cin  >> playerNum;
-        cout << "" << endl
-             << "You have chosen the number: " << playerNum << endl
-             << "" << endl;
+          // Depending on type of play chosen program proceeds diffrently
+          if (playType == "color")
+               {
+                    cout << "Select a color to bet on " << endl
+                         << "   To bet on Black type: black" << endl
+                         << "   To bet on Red   type: red" << endl;
+                    cin >> playerCol;
+                    if (cin.eof())
+                         return 0; // Let user shut down program
+                    cout << "" << endl
+                         << "You have chosen the color :" << playerCol << endl
+                         << "" << endl;
+               }
+               else if (playType == "number")
+                    {
+                         cout << "Select a number to bet on " << endl
+                              << "Type a number between 1 and 36: " << endl;
+                         cin >> playerNum;
+                         if (cin.eof())
+                              return 0; // Let user shut down program
+                         cout << "" << endl
+                              << "You have chosen the number: " << playerNum << endl
+                              << "" << endl;
+                    }
+                    else
+                         {
+                              cout << "invalid type of play " << endl
+                                   << "Type: " << endl
+                                   << "   color   | to bet on color " << endl
+                                   << "   number  | to bet on number " << endl;
+                              cin >> playType;
+                              if (cin.eof())
+                                   return 0; // Let user shut down program
+                         }
 
-    } else {
-        cout << "invalid type of play " << endl
-             << "Type: " << endl
-             << "   color   | to bet on color " << endl
-             << "   number  | to bet on number " << endl;
-        cin  >> playType;
-    }
+          // Let user place a bet on type of play
+          cout << "You have: " << wallet << " kr how much do you want to bet? " << endl
+               << "Type:  " << endl
+               << "100    | to bet 100kr on play" << endl
+               << "300    | to bet 300kr on play" << endl
+               << "500    | to bet 500kr on play" << endl;
 
-    // Promt player for amout to bet on play
-    cout << "You have: " << wallet << " kr how much do you want to bet? " << endl
-         << "Type:  "
-         <<     "100    | to bet 100kr on play" << endl
-         <<     "300    | to bet 300kr on play" << endl
-         <<     "500    | to bet 500kr on play" << endl;
-    
-    // Store bet
-    cin  >> bet;
+          // Let user input bet and stores value
+          cin >> bet;
+          if (cin.eof())
+               return 0; // Let user shut down program
 
-    // Check if player can afford bet.
-    if (bet > wallet) {
-        cout << "You do not have enough cash to place that bet " << endl
-             << "You have: " << wallet << " kr how much do you want to bet? " << endl
-         << "Type:  "
-         <<     "100    | to bet 100kr on play" << endl
-         <<     "300    | to bet 300kr on play" << endl
-         <<     "500    | to bet 500kr on play" << endl;
-    } else {
-        cout << "Bet is placed are you ready to roll the wheel? " << endl;
-    }
+          // Control that makes sure the user can afford chosen bet
+          if (bet > wallet)
+               {
+                    cout << "You do not have enough cash to place that bet " << endl
+                         << "You have: " << wallet << " kr how much do you want to bet? " << endl
+                         << "Type:  "
+                         << "100    | to bet 100kr on play" << endl
+                         << "300    | to bet 300kr on play" << endl
+                         << "500    | to bet 500kr on play" << endl;
+               }
+               // Let user proceed after control of bet
+               else
+                    {
+                         cout << "Bet is placed are you ready to roll the wheel? " << endl;
+                    }
 
-    cout << "Type: spin     | to roll the wheel" << endl;
+          cout << "Type: spin     | to roll the wheel" << endl;
+          cin >> spin;
+          if (cin.eof())
+               return 0; // Let user shut down program
 
+          if (spin == "spin")
+          {
+               // Check if user typed "spin" and if so spin the wheel
+               srand(time(0));
+               int resultNum = rand() % 36 + 1;
+               string resultCol = (resultNum % 2 == 0) ? "black" : "red"; // color logging
 
-    return 0;
+               cout << "The wheel landed on " << resultNum << " " << resultCol << endl;
+
+               // Check if user has played on color and proceed with win or loose
+               if (playType == "color")
+               {
+                    if (playerCol == resultCol)
+                    {
+                         wallet += bet;
+                         cout << "Congratulations! you won, your new balance is: " << wallet << " kr" << endl;
+                    }
+                    else 
+                    {
+                         wallet -= bet;
+                         cout << "sorry you lost. your new balance is: " << wallet << " kr" << endl;
+                    }
+               }
+
+               // Check if user has played on number and proceed with win or loose 
+               else if (playType == "number") 
+               {
+                    if (playerNum == resultNum) 
+                    {
+                         wallet += bet * 10;
+                         cout << "Congratulations! you won. Your new wallet balance is: " << wallet << " kr" << endl;
+                    } 
+                    else 
+                    {
+                    wallet -= bet;
+                    cout << "Sorry, you lost. Your new balance is: " << wallet << " kr" << endl;
+                    }       
+               }
+     
+          } 
+          else 
+          {
+               cout << "Invalid command type: spin to roll " << endl;
+               continue;
+          }
+          // Check if there is balance in wallet before letting user play again
+          if (wallet <= 0) 
+          {
+               cout << "You have run out of money. Game OVER!" << endl;
+               return 0;
+          }
+
+          // ask user to play another round or not
+          cout << "Do you want to play again? (yes/no)" << endl;
+          
+          // Store value for play again or not
+          cin >> playAgain;
+          if (cin.eof())
+               return 0; // Shut the program down
+
+          // Check if user typed yes to play again otherwise shut program down
+          if (playAgain != "yes") 
+          {
+               cout << "Thank you for playing! Goodbey!" << endl;
+               return 0;
+          }
+     }
+     
+     return 0;
 }
